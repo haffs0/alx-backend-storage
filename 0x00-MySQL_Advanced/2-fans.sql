f89f8b717962e69407813c8ba84970fe8a70f946
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-04-06 09:10:19 +0000

    rank country origin

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- script that ranks country origins
-SELECT orgin, SUM(fans) AS nb_fans FROM metal_bands
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
